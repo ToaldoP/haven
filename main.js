f25496903ae935e94a6ce1a3f85462c90cdd5e48
Toaldop <toaldofpedroso@gmail.com>
2021-11-21 03:33:28 -0300

    finalização do layout no figma. Inicio de JavaScript

--- main.js
+++ main.js
@@ -1,18 +1,67 @@
-const shareButton = document.getElementById('open-share-modal')
-let shareModal = document.getElementsByClassName('share-modal')
-
-shareButton.addEventListener('click', function () {
-  document.querySelector('.share-modal').style.visibility = 'visible'
-})
+/*CLOSE OVERLAY*/
+const modalOverlay = document.querySelector('.modal-overlay')
 
 document.addEventListener('click', function (event) {
-  if (!event.target.closest('.share-modal')) {
+  let clickOutside = modalOverlay.contains(event.target)
+  if (clickOutside) {
+    closeOverlay()
+  }
+})
+
+function closeOverlay() {
+  modalOverlay.classList.remove('show')
+}
+
+/*OPEN AND CLOSE CONTACT MODAL*/
+const contactButton = document.querySelector('#contact-button')
+const contactModal = document.querySelector('#contact-modal')
+
+contactButton.addEventListener('click', openContact)
+
+function openContact(event) {
+  contactModal.classList.add('show')
+  modalOverlay.classList.add('show')
+  event.stopImmediatePropagation()
+}
+
+document.addEventListener('click', function (event) {
+  let clickOutside = !contactModal.contains(event.target)
+  if (clickOutside) {
+    closeContactModal()
+  }
+})
+
+function closeContactModal() {
+  contactModal.classList.remove('show')
+}
+
+/*OPEN AND CLOSE SHARE MODAL*/
+const shareButton = document.querySelector('#open-share-modal')
+const shareModal = document.querySelector('#share-modal')
+
+shareButton.addEventListener('click', openShare)
+
+function openShare(event) {
+  shareModal.classList.add('show')
+  modalOverlay.classList.add('show')
+  event.stopImmediatePropagation()
+}
+
+/* close */
+document.addEventListener('click', function (event) {
+  let clickOutside = !shareModal.contains(event.target)
+  if (clickOutside) {
     closeShareModal()
   }
-},
-false
-)
+})
 
 function closeShareModal() {
-  document.querySelector('.share-modal').style.visibility = 'hidden'
+  shareModal.classList.remove('show')
 }
+
+document.addEventListener('click', function (event) {
+  let clickOutside = !shareModal.contains(event.target)
+  if (clickOutside) {
+    closeShareModal()
+  }
+})
