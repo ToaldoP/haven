cee708b74c1d90ed85c927a646069bf9c0a582c6
Toaldop <toaldofpedroso@gmail.com>
2021-11-30 03:39:55 -0300

    gallery modal e remoção do jquery

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 /*CLOSE OVERLAY*/
 const modalOverlay = document.querySelector('.modal-overlay')
+const galleryOverlay = document.querySelector('.gallery-overlay')
 
 document.addEventListener('click', function (event) {
   let clickOutside = modalOverlay.contains(event.target)
@@ -8,9 +9,21 @@ document.addEventListener('click', function (event) {
   }
 })
 
+document.addEventListener('click', function (event) {
+  let clickOutside = galleryOverlay.contains(event.target)
+  if (clickOutside) {
+    closeOverlay()
+  }
+})
+
 function closeOverlay() {
   modalOverlay.classList.remove('show')
+  galleryOverlay.classList.remove('show')
 }
+
+
+
+
 
 /*OPEN AND CLOSE CONTACT MODAL*/
 const contactButton = document.querySelector('#contact-button')
@@ -34,6 +47,10 @@ document.addEventListener('click', function (event) {
 function closeContactModal() {
   contactModal.classList.remove('show')
 }
+
+
+
+
 
 /*OPEN AND CLOSE SHARE MODAL*/
 const shareButton = document.querySelector('#open-share-modal')
@@ -66,10 +83,13 @@ document.addEventListener('click', function (event) {
   }
 })
 
+
+
+
+
 /*OPEN AND CLOSE SHARE MODAL*/
 const moreInfosButton = document.querySelector('#more-infos')
 const moreInfosModal = document.querySelector('#more-infos-modal')
- 
 
 moreInfosButton.addEventListener('click', openInfosModal)
 
@@ -89,3 +109,51 @@ document.addEventListener('click', function (event) {
     closeMoreInfosModal()
   }
 })
+
+
+
+
+
+/*OPEN AND CLOSE GALLERY MODAL & FAVORITE INTERACTION*/
+const galleryButton = document.querySelector('.main-photo')
+const galleryModal = document.querySelector('#gallery-modal')
+const favoriteButton = document.querySelector('.favorite-button')
+
+
+favoriteButton.addEventListener('click', alerta)
+
+function alerta(){
+  console.log('favorito')
+  event.stopImmediatePropagation()
+}
+
+
+galleryButton.addEventListener('click', openGalleryModal)
+
+function openGalleryModal(event) {
+  galleryModal.classList.add('show')
+  galleryOverlay.classList.add('show')
+
+  event.stopImmediatePropagation()
+}
+
+
+function closeGalleryModal() {
+  galleryModal.classList.remove('show')
+
+}
+
+document.addEventListener('click', function (event) {
+  let clickOutside = !galleryModal.contains(event.target)
+  if (clickOutside) {
+    closeGalleryModal()
+  }
+})
+
+
+
+
+
+
+
+
